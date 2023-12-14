e1464ca6ad40d6f36db765ed3eb70bb02e361cab
Mark Embling <mark@markembling.info>
2023-12-14 16:28:15 +0000

    Update refund URLs

--- src/SagePayMvc/Configuration.cs
+++ src/SagePayMvc/Configuration.cs
@@ -39,8 +39,8 @@ namespace SagePayMvc {
 		public const string TestUrl = "https://sandbox.opayo.eu.elavon.com/gateway/service/vspserver-register.vsp";
 		public const string SimulatorUrl = "https://test.sagepay.com/simulator/VSPServerGateway.asp?Service=VendorRegisterTx";
 
-		public const string LiveRefundUrl = "https://live.sagepay.com/gateway/service/refund.vsp";
-		public const string TestRefundUrl = "https://test.sagepay.com/gateway/service/refund.vsp";
+		public const string LiveRefundUrl = "https://live.opayo.eu.elavon.com/gateway/service/refund.vsp";
+		public const string TestRefundUrl = "https://sandbox.opayo.eu.elavon.com/gateway/service/refund.vsp";
 		public const string SimulatorRefundUrl = "https://test.sagepay.com/simulator/vspserverGateway.asp?Service=VendorRefundTx";
 
 		string notificationController = DefaultControllerName;
